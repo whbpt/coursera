ec31d63516bf0510cf993a19b2305cbc8d568b06
wwwhbpt@gmail.com <wwwhbpt@gmail.com>
2016-07-21 09:59:10 +0800

    4-5

--- data_structures_and_algorithms/4_dynamic_programming_starter_files/lcs3/lcs3.py
+++ data_structures_and_algorithms/4_dynamic_programming_starter_files/lcs3/lcs3.py
@@ -1,11 +1,25 @@
 #Uses python3
 
 import sys
-
+import numpy as np
+def edit_distance(ss, tt, kk):
+    global big_
+    slen=len(ss)
+    tlen=len(tt)
+    klen=len(kk)
+    array_=np.zeros([slen+1,tlen+1,klen+1])
+    for i in range(1,slen+1):
+        for j in range(1,tlen+1):
+            for k in range(1,klen+1):
+                if ss[i-1]==tt[j-1] and tt[j-1]==kk[k-1]:
+                    array_[i,j,k]=array_[i-1,j-1,k-1]+1
+                else:
+                    array_[i,j,k]=max(array_[i-1,j,k],array_[i,j-1,k],array_[i,j,k-1])
+    return int(array_[slen,tlen,klen])
 def lcs3(a, b, c):
     #write your code here
-    return min(len(a), len(b), len(c))
-
+    temp_=edit_distance(a,b,c)
+    return temp_
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
